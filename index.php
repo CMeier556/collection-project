ef0882fcf6eac0ac95fc6e543b85ff9c9ba2a406
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-07-31 14:20:13 +0100

    Peer Review Changes

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 $db = new PDO('mysql:host=db;dbname=project2collector', 'root', 'password');
-$db→setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $query = $db->prepare("SELECT `name`, `definition`, `synonyms`, `example_sentence` FROM `collection`");
 $query->execute();
 
 
-$data = $query->fetchAll();
+$word_collection = $query->fetchAll();
 
 echo '<pre>';
-var_dump ($data);
+var_dump ($word_collection);
 echo '</pre>';
