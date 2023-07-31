80b46ef1ae22ba552e22d27a9cb62916911697fd
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-07-31 16:10:24 +0100

    Started HTML

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
-<?php
-$db = new PDO('mysql:host=db;dbname=project2collector', 'root', 'password');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+<?php require_once('collection.php') ?>
 
-$query = $db->prepare("SELECT `name`, `definition`, `synonyms`, `example_sentence` FROM `collection`");
-$query->execute();
+<!DOCTYPE html>
 
+<html>
+<head>
+    <title>Colin's Collection</title>
+</head>
+<body>
+    <h1>Welcome to Colin's Word Collection!</h1>
+    <p>Here are some of my favorite words from the dictionary</p>
+    <div class="word">
+        <p>Word 1 - Definition</p>
 
-$word_collection = $query->fetchAll();
-
-echo '<pre>';
-var_dump ($word_collection);
-echo '</pre>';
+</body>
+</html>
