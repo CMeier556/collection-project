75ade7cb5417b6e2b2fbfd92185d8768cbb3cb23
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-07-31 14:09:30 +0100

    Displayed data array on webpage

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+$db = new PDO('mysql:host=db;dbname=project2collector', 'root', 'password');
+$db→setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare("SELECT `name`, `definition`, `synonyms`, `example_sentence` FROM `collection`");
+$query->execute();
+
+
+$data = $query->fetchAll();
+
+echo '<pre>';
+var_dump ($data);
+echo '</pre>';
