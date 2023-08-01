96531501cfd34fdec264580cd44afe8ce3e2e66d
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-01 12:09:34 +0100

    Linked Database info to page

--- index.php
+++ index.php
@@ -1,46 +1,28 @@
-<?php require_once('collection.php') ?>
+<?php require_once('collection.php'); 
+$db = new PDO('mysql:host=db;dbname=project2collector', 'root', 'password');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare("SELECT `name`, `definition`, `synonyms`, `example_sentence` FROM `collection`");
+$query->execute();
+$word_collection = $query->fetchAll();
+?>
 
 <!DOCTYPE html>
 
 <html>
-<head>
-    <title>Colin's Collection</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <h1>Welcome to Colin's Word Collection!</h1>
-    <p>Here are some of my favorite words from the dictionary.</p>
-      <section class="word-container">      
-        <div class="item">
-                <h3 class="word">Word</h3>
-                <p class="stat">Definition:</p>
-                <p class="stat">Synonyms:</p>
-                <p class="stat">Example Sentence:</p>
-            </div>
-        </section>
-        <section class="word-container">      
-        <div class="item">
-                <h3 class="word">Word</h3>
-                <p class="stat">Definition:</p>
-                <p class="stat">Synonyms:</p>
-                <p class="stat">Example Sentence:</p>
-            </div>
-        </section>
-        <section class="word-container">      
-        <div class="item">
-                <h3 class="word">Word</h3>
-                <p class="stat">Definition:</p>
-                <p class="stat">Synonyms:</p>
-                <p class="stat">Example Sentence:</p>
-            </div>
-        </section>
-        <section class="word-container">      
-        <div class="item">
-                <h3 class="word">Word</h3>
-                <p class="stat">Definition:</p>
-                <p class="stat">Synonyms:</p>
-                <p class="stat">Example Sentence:</p>
-            </div>
-        </section>
-</body>
+    <head>
+        <title>Colin's Collection</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h1>Welcome to Colin's Word Collection!</h1>
+        <p>Here are some of my favorite words from the dictionary.</p>
+        <?php 
+        foreach ($word_collection as $word) {
+            $word = new Word ($word['name'], $word['definition'], $word['synonyms'], $word['example_sentence']);
+            echo ($word->createWordCard());
+        }
+    ?>
+    </body>
 </html>
