96003631673d1d598d7bf2240a5d36b4f0e89dee
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-01 14:04:36 +0100

    Changes

---
+++ Word.php
@@ -0,0 +1,28 @@
+<?php
+
+
+ class Word {
+    public string $name;
+    public string $definition;
+    public string $synonyms;
+    public string $sentence;
+
+    public function __construct (string $name, string $definition, string $synonyms, string $sentence)
+    {
+        $this->name=$name;
+        $this->definition=$definition;
+        $this->synonyms=$synonyms;
+        $this->sentence=$sentence;
+    }
+
+    public function createWordCard() {
+        return "<section class='word-container'>      
+                    <div class='item'>
+                            <h3 class='word'>$this->name</h3>
+                            <p class='stat'><b>Definition</b>: $this->definition</p>
+                            <p class='stat'><b>Synonyms</b>:  $this->synonyms</p>
+                            <p class='stat'><b>Example Sentence</b>: $this->sentence</p>
+                        </div>
+                </section>";
+    }
+}   
