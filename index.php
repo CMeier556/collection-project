d3d9675e0884f9ff8f460a9372b4eaffb7066545
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-01 10:08:40 +0100

    Added boxes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <h1>Welcome to Colin's Word Collection!</h1>
-    <p>Here are some of my favorite words from the dictionary</p>
+    <p>Here are some of my favorite words from the dictionary.</p>
       <section class="word-container">      
         <div class="item">
                 <h3 class="word">Word</h3>
@@ -17,6 +17,30 @@
                 <p class="stat">Synonyms:</p>
                 <p class="stat">Example Sentence:</p>
             </div>
-    </section>
+        </section>
+        <section class="word-container">      
+        <div class="item">
+                <h3 class="word">Word</h3>
+                <p class="stat">Definition:</p>
+                <p class="stat">Synonyms:</p>
+                <p class="stat">Example Sentence:</p>
+            </div>
+        </section>
+        <section class="word-container">      
+        <div class="item">
+                <h3 class="word">Word</h3>
+                <p class="stat">Definition:</p>
+                <p class="stat">Synonyms:</p>
+                <p class="stat">Example Sentence:</p>
+            </div>
+        </section>
+        <section class="word-container">      
+        <div class="item">
+                <h3 class="word">Word</h3>
+                <p class="stat">Definition:</p>
+                <p class="stat">Synonyms:</p>
+                <p class="stat">Example Sentence:</p>
+            </div>
+        </section>
 </body>
 </html>
