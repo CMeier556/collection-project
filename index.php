7e6b47e75a21640519354a8346d58f5c1e33ef6b
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-08-01 15:12:55 +0100

    CSS Changes

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once('collection.php'); 
+<?php require_once('Word.php'); 
 $db = new PDO('mysql:host=db;dbname=project2collector', 'root', 'password');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
